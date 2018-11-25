647a029b329790f5c982b7e2c357a5238d686299
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-11-25 13:49:41 +0200

    wip

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../css/img/logo.svg';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="ui inverted vertical center aligned segment">
+          <img src={logo} className="App-logo" alt="TVMaze" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+          TVMaze
+          </a>
+      </header>
+    )
+  }
+}
+
+export default Header;
