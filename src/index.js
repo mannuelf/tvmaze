7e0a0467170e865d9b1a2c5dca376fa986dc0c9d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-11-25 12:44:43 +0200

    organise project structure

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import Route from './Router'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
